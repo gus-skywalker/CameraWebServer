3d5a2c505e487ce7fa7379035d2815e89400fb2f
Guga Dam <lesmonades@gmail.com>
2025-06-26 00:37:44 -0300

    open interfaces for http

--- server/server.js
+++ server/server.js
@@ -26,4 +26,4 @@ wsServer.on('connection', (ws, req)=>{
 });
 
 app.get('/client',(req,res)=>res.sendFile(path.resolve(__dirname, './client.html')));
-app.listen(HTTP_PORT, ()=> console.log(`HTTP server listening at ${HTTP_PORT}`));
+app.listen(HTTP_PORT, '0.0.0.0', () => console.log(`HTTP server listening at ${HTTP_PORT}`));
